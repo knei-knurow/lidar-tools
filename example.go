c42b074dbea667d732ad42e294392ddca8d26380
Bartek Pacia <barpac02@gmail.com>
2021-05-24 15:55:51 +0200

    update example

--- example.go
+++ example.go
@@ -7,8 +7,10 @@ import (
 )
 
 func main() {
+	// Demonstration of frame.CalculateCRC and frame.DescribeByte functions.
 	data := []byte{123, 153, 223}
 	fmt.Printf("data: %03b, crc: %s\n", data, frame.DescribeByte(frame.CalculateCRC(data)))
+
 	// Demonstration of frame.DescribeByte function.
 	// It's very useful to quickly examine what a particular byte means.
 	// See its implementation to learn few cool tricks about formatting verbs.
