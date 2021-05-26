4742959dd7a9bcf35a3e380316bd79031675ff71
Bartek Pacia <barpac02@gmail.com>
2021-05-26 11:41:16 +0200

    add flag to wait for a single byte of response

--- cmd/servo/servo.go
+++ cmd/servo/servo.go
@@ -19,9 +19,10 @@ var (
 )
 
 var (
-	value    int
-	minValue uint
-	maxValue uint
+	value           int
+	minValue        uint
+	maxValue        uint
+	waitForResponse bool
 )
 
 func init() {
@@ -34,6 +35,7 @@ func init() {
 	flag.IntVar(&value, "value", -1, "value to encode into a frame and send")
 	flag.UintVar(&minValue, "min-value", 1600, "minimum value that is valid (uint16")
 	flag.UintVar(&maxValue, "max-value", 4400, "maximum value that is valid (uint16")
+	flag.BoolVar(&waitForResponse, "wait", false, "whether to wait for the MCU to respond with a single byte")
 }
 
 func main() {
@@ -50,40 +52,52 @@ func main() {
 
 	port, err := serial.Open(options)
 	if err != nil {
-		log.Fatalf("failed to open serial port %s: %v\n", portName, err)
+		log.Fatalf("servo: failed to open serial port %s: %v\n", portName, err)
 	}
 	defer port.Close()
 
 	if value == -1 {
-		fmt.Println("uart_echo: finish: -1 entered")
+		fmt.Println("servo: finish: -1 entered")
 		os.Exit(0)
 	}
 
 	if value < int(minValue) {
-		log.Fatalf("uart_echo: error: %d is smaller than %d\n", value, minValue)
+		log.Fatalf("servo: error: %d is smaller than %d\n", value, minValue)
 	}
 
 	if value > int(maxValue) {
-		log.Fatalf("uart_echo: error: %d is bigger than max value %d\n", value, maxValue)
+		log.Fatalf("servo: error: %d is bigger than max value %d\n", value, maxValue)
 	}
 
 	if value > 65535 {
-		log.Fatalf("uart_echo: error: %d overflows uint16\n", value)
+		log.Fatalf("servo: error: %d overflows uint16\n", value)
 	}
 
 	inputByte := uint16(value)
 	f := frame.EncodeFrame(inputByte)
 
-	fmt.Printf("frame: %s\n", f)
+	fmt.Printf("servo: frame: %s\n", f)
 	for i, currentByte := range f {
 		fmt.Println("---")
-		fmt.Printf("%d %s will be sent\n", i, frame.DescribeByte(currentByte))
+		fmt.Printf("servo: %d %s will be sent\n", i, frame.DescribeByte(currentByte))
 		_, err := port.Write([]byte{currentByte})
 		if err != nil {
-			log.Fatalf("%d byte: failed to write it to serial port: %v\n", i, err)
+			log.Fatalf("servo: %d byte: failed to write it to serial port: %v\n", i, err)
 		}
-		fmt.Printf("%d byte: wrote it to serial port\n", i)
+		fmt.Printf("servo: %d byte: wrote it to serial port\n", i)
 	}
 
-	fmt.Println("uart_echo: finish")
+	if waitForResponse {
+		fmt.Printf("servo: waiting for single byte...\n")
+		output := make([]byte, 1)
+		n, err := port.Read(output)
+		if err != nil {
+			log.Fatalln("servo: failed to read from serial port:", err)
+		}
+		outputByte := output[0]
+
+		fmt.Printf("servo: read %d bytes (\"%d\") from serial port \n", n, outputByte)
+	}
+
+	fmt.Println("servo: finish")
 }
