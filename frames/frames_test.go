8ac4ee045366790ed7ead0091286aa5cef495327
Bartek Pacia <barpac02@gmail.com>
2021-05-31 01:58:04 +0200

    improve frames_test.go code

--- frames/frames_test.go
+++ frames/frames_test.go
@@ -8,127 +8,85 @@ import (
 	"github.com/knei-knurow/lidar-tools/frames"
 )
 
-func TestCreate(t *testing.T) {
-	testCases := []struct {
-		inputHeader      []byte
-		inputData        []byte
-		expectedChecksum byte
-	}{
-		{
-			inputHeader:      []byte{'L', 'D'},
-			inputData:        []byte{},
-			expectedChecksum: 0x00,
-		},
-		{
-			inputHeader:      []byte{'L', 'D'},
-			inputData:        []byte{'A'},
-			expectedChecksum: 0x41,
-		},
-		{
-			inputHeader:      []byte{'L', 'D'},
-			inputData:        []byte{'t', 'e', 's', 't'},
-			expectedChecksum: 0x16,
-		},
-		{
-			inputHeader:      []byte{'L', 'D'},
-			inputData:        []byte{'d', 'u', 'p', 'c', 'i', 'a'},
-			expectedChecksum: 0x0a,
-		},
-		{
-			inputHeader:      []byte{'L', 'D'},
-			inputData:        []byte{'l', 'o', 'l', 'x', 'd'},
-			expectedChecksum: 0x73,
-		},
-		{
-			inputHeader:      []byte{'B', 'I', 'G'},
-			inputData:        []byte{'d', 'o', 'n', 'd', 'u'},
-			expectedChecksum: 0x30,
-		},
-		{
-			inputHeader:      []byte{},
-			inputData:        []byte{},
-			expectedChecksum: 0x08,
-		},
-	}
+var testCases = []struct {
+	header   []byte
+	data     []byte
+	checksum byte
+}{
+	{
+		header:   []byte{'L', 'D'},
+		data:     []byte{},
+		checksum: 0x00,
+	},
+	{
+		header:   []byte{'L', 'D'},
+		data:     []byte{'A'},
+		checksum: 0x41,
+	},
+	{
+		header:   []byte{'L', 'D'},
+		data:     []byte{'t', 'e', 's', 't'},
+		checksum: 0x16,
+	},
+	{
+		header:   []byte{'L', 'D'},
+		data:     []byte{'d', 'u', 'p', 'c', 'i', 'a'},
+		checksum: 0x0a,
+	},
+	{
+		header:   []byte{'L', 'D'},
+		data:     []byte{'l', 'o', 'l', 'x', 'd'},
+		checksum: 0x73,
+	},
+	{
+		header:   []byte{'B', 'I', 'G'},
+		data:     []byte{'d', 'o', 'n', 'd', 'u'},
+		checksum: 0x30,
+	},
+	{
+		header:   []byte{},
+		data:     []byte{},
+		checksum: 0x08,
+	},
+}
 
+func TestCreate(t *testing.T) {
 	for i, tc := range testCases {
 		testName := fmt.Sprintf("test %d", i)
 		t.Run(testName, func(t *testing.T) {
-			gotFrame := frames.Create(tc.inputHeader, tc.inputData)
+			gotFrame := frames.Create(tc.header, tc.data)
 
-			if !bytes.Equal(gotFrame.Header(), tc.inputHeader) {
-				t.Errorf("got header % x, want header % x", gotFrame.Header(), tc.inputHeader)
+			if !bytes.Equal(gotFrame.Header(), tc.header) {
+				t.Errorf("got header % x, want header % x", gotFrame.Header(), tc.header)
 			}
 
-			if !bytes.Equal(gotFrame.Data(), tc.inputData) {
-				t.Errorf("got data % x, want data % x", gotFrame.Data(), tc.inputData)
+			if !bytes.Equal(gotFrame.Data(), tc.data) {
+				t.Errorf("got data % x, want data % x", gotFrame.Data(), tc.data)
 			}
 
-			if gotFrame.Checksum() != tc.expectedChecksum {
-				t.Errorf("got checksum % x, want checksum % x", gotFrame.Checksum(), tc.expectedChecksum)
+			if gotFrame.Checksum() != tc.checksum {
+				t.Errorf("got checksum % x, want checksum % x", gotFrame.Checksum(), tc.checksum)
 			}
 		})
 	}
 }
 
 func TestAssemble(t *testing.T) {
-	testCases := []struct {
-		inputHeader   []byte
-		inputData     []byte
-		inputChecksum byte
-	}{
-		{
-			inputHeader:   []byte{'L', 'D'},
-			inputData:     []byte{},
-			inputChecksum: 0x00,
-		},
-		{
-			inputHeader:   []byte{'L', 'D'},
-			inputData:     []byte{'A'},
-			inputChecksum: 0x41,
-		},
-		{
-			inputHeader:   []byte{'L', 'D'},
-			inputData:     []byte{'t', 'e', 's', 't'},
-			inputChecksum: 0x16,
-		},
-		{
-			inputHeader:   []byte{'L', 'D'},
-			inputData:     []byte{'d', 'u', 'p', 'c', 'i', 'a'},
-			inputChecksum: 0x0a,
-		},
-		{
-			inputHeader:   []byte{'L', 'D'},
-			inputData:     []byte{'l', 'o', 'l', 'x', 'd'},
-			inputChecksum: 0x73,
-		},
-		{
-			inputHeader:   []byte{'B', 'I', 'G'},
-			inputData:     []byte{'d', 'o', 'n', 'd', 'u'},
-			inputChecksum: 0x30,
-		},
-		{
-			inputHeader:   []byte{},
-			inputData:     []byte{},
-			inputChecksum: 0x08,
-		},
-	}
-
 	for i, tc := range testCases {
 		testName := fmt.Sprintf("test %d", i)
 		t.Run(testName, func(t *testing.T) {
-			gotFrame := frames.Create(tc.inputHeader, tc.inputData)
+			gotFrame := frames.Assemble(tc.header, tc.data, tc.checksum)
 
-			if !bytes.Equal(gotFrame.Header(), tc.inputHeader) {
-				t.Errorf("got header % x, want header % x", gotFrame.Header(), tc.inputHeader)
+			if !bytes.Equal(gotFrame.Header(), tc.header) {
+				t.Errorf("got header % x, want header % x", gotFrame.Header(), tc.header)
 			}
 
-			if !bytes.Equal(gotFrame.Data(), tc.inputData) {
-				t.Errorf("got data % x, want data % x", gotFrame.Data(), tc.inputData)
+			if !bytes.Equal(gotFrame.Data(), tc.data) {
+				t.Errorf("got data % x, want data % x", gotFrame.Data(), tc.data)
 			}
 
-			if gotFrame.Checksum() != tc.inputChecksum {
-				t.Errorf("got checksum % x, want checksum % x", gotFrame.Checksum(), tc.inputChecksum)
+			if gotFrame.Checksum() != tc.checksum {
+				t.Errorf("got checksum % x, want checksum % x", gotFrame.Checksum(), tc.checksum)
 			}
 		})
 	}
