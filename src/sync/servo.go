7230991644c9e9913fe98e515dc32618132c5c2e
Szymon Bednorz <dsonyyy@gmail.com>
2021-09-06 22:35:55 +0200

    scanning

--- src/sync/servo.go
+++ src/sync/servo.go
@@ -41,9 +41,11 @@ func (servo *Servo) Move() {
 	case servo.data.positon < servo.positonMin:
 		servo.data.positon = servo.positonMin
 		servo.vector = -servo.vector
+		log.Println("servo min position reached")
 	case servo.data.positon > servo.positonMax:
 		servo.data.positon = servo.positonMax
 		servo.vector = -servo.vector
+		log.Println("servo max position reached")
 	}
 }
 
